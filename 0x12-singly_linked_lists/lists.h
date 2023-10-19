e40bb316ec4b087b0d3f28b53ab7e3f5ca9f9220
MarinaAzem9 <marina.azem9@gmail.com>
2023-10-19 12:17:12 -0700

    C - Singly linked lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,59 +2,25 @@
 #define LISTS_H
 
 /**
- * struct Node - singly linked list node
- * @data: string - (malloc'ed string)
- * @length: length of the string
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
  * @next: points to the next node
  *
- * Description: Node structure for a singly linked list
- * used in a program.
+ * Description: singly linked list node structure
+ * for Holberton project
  */
-typedef struct Node
+typedef struct list_s
 {
-	char *data;
-	unsigned int length;
-	struct Node *next;
-} Node_t;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
-/**
- * print_linked_list - Prints all the elements of a linked list.
- * @head: Pointer to the head of the linked list.
- *
- * Return: The number of nodes in the linked list.
- */
-size_t print_linked_list(const Node_t *head);
-
-/**
- * linked_list_length - Calculates the number of nodes in a linked list.
- * @head: Pointer to the head of the linked list.
- *
- * Return: The number of nodes in the linked list.
- */
-size_t linked_list_length(const Node_t *head);
-
-/**
- * add_node_to_list - Adds a new node at the beginning of a linked list.
- * @head: Pointer to the head of the linked list.
- * @str: The string to be stored in the new node.
- *
- * Return: The address of the new node, or NULL if it fails.
- */
-Node_t *add_node_to_list(Node_t **head, const char *str);
-
-/**
- * add_node_to_end - Adds a new node at the end of a linked list.
- * @head: Pointer to the head of the linked list.
- * @str: The string to be stored in the new node.
- *
- * Return: The address of the new node, or NULL if it fails.
- */
-Node_t *add_node_to_end(Node_t **head, const char *str);
-
-/**
- * free_linked_list - Frees the memory occupied by a linked list.
- * @head: Pointer to the head of the linked list.
- */
-void free_linked_list(Node_t *head);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
